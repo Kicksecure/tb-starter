a30e45669474764b5aa27f70225cfcf3937abd6a
idk <problemsolver@openmailbox.org>
2018-10-02 13:17:04 -0400

    added in alternate profile and alternate profile launcher

---
+++ usr/share/tb-profile-i2p/profile.i2p/preferences/i2p-browser.js
@@ -0,0 +1,14 @@
+
+pref("network.proxy.no_proxies_on", 0);
+pref("network.proxy.type", 1);
+pref("network.proxy.http", "127.0.0.1");
+pref("network.proxy.http_port", 4444);
+pref("network.proxy.ssl", "127.0.0.1");
+pref("network.proxy.ssl_port", 4444);
+pref("network.proxy.ftp", "127.0.0.1");
+pref("network.proxy.ftp_port", 4444);
+pref("network.proxy.socks", "127.0.0.1");
+pref("network.proxy.socks_port", 4444);
+pref("network.proxy.share_proxy_settings", true);
+pref("browser.startup.homepage", "about:blank");
+
