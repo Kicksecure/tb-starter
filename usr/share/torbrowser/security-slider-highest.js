b3e8dc165ce9b0c728c80fa9759afa5bc3b69a77
Patrick Schleizer <adrelanos@riseup.net>
2019-07-12 15:24:44 -0400

    add first start popup to ask whether security slider should be set to safest
    https://forums.whonix.org/t/ship-an-additional-version-of-tor-browser-with-security-slider-settings-to-high-by-default/7591

---
+++ usr/share/torbrowser/security-slider-highest.js
@@ -0,0 +1,6 @@
+// Copyright (C) 2019 - 2019 ENCRYPTED SUPPORT LP <adrelanos@riseup.net>
+// See the file COPYING for copying conditions.
+
+// Set security slider "Safest".
+user_pref("extensions.torbutton.inserted_security_level", true);
+user_pref("extensions.torbutton.security_slider", 1);
