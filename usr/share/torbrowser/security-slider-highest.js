a92a4ced3ee92d41685b247ffcd218c001d1cd5b
Patrick Schleizer <adrelanos@riseup.net>
2019-09-19 04:21:11 -0400

    comment

--- usr/share/torbrowser/security-slider-highest.js
+++ usr/share/torbrowser/security-slider-highest.js
@@ -4,6 +4,10 @@
 // If user chooses so, this file gets copied at first start to:
 // ~/.torbrowser/torbrowser/Browser/TorBrowser/Data/Browser/profile.default/user.js
 
+// [Feature Request] Environment Variable to set security slider level
+// closed enhancement (wontfix)
+// https://trac.torproject.org/projects/tor/ticket/25391
+
 // Set security slider "Safest".
 user_pref("extensions.torbutton.inserted_security_level", true);
 user_pref("extensions.torbutton.security_slider", 1);
